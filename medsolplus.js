bd139521ce0139b58e6f17624422824af9858f0e
Dr. Galambos Máté <thermate18@gmail.com>
2023-06-16 11:12:23 +0200

    Bump version

--- medsolplus.js
+++ medsolplus.js
@@ -7,7 +7,7 @@
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM.xmlHttpRequest
-// @version     1.04
+// @version     1.05
 // @downloadURL https://raw.githubusercontent.com/matega/medsolplus/master/medsolplus.js
 // @author      Dr. Galambos Máté | galambos.mate@semmelweis.hu
 // @description e-MedSolution extra funkciók a sürgősségi osztályon (KSBA)
